393169b9bb1777995232668aeb950587b43d969c
sabilimaulana <spacysbl.learn@gmail.com>
2021-12-29 20:57:23 +0700

    Slicing : Detail Chat Page Bagian Input 1

--- lib/pages/detail_chat_page.dart
+++ lib/pages/detail_chat_page.dart
@@ -46,9 +46,47 @@ class DetailChatPage extends StatelessWidget {
       );
     }
 
+    Widget chatInput() {
+      return Container(
+        margin: const EdgeInsets.all(20),
+        child: Row(
+          children: [
+            Expanded(
+              child: Container(
+                height: 40,
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 16,
+                ),
+                decoration: BoxDecoration(
+                  color: backgroundColor4,
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: Center(
+                  child: TextFormField(
+                    decoration: InputDecoration.collapsed(
+                      hintText: 'Type Message...',
+                      hintStyle: subtitleTextStyle,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(
+              width: 20,
+            ),
+            Image.asset(
+              'assets/button_send.png',
+              width: 45,
+            )
+          ],
+        ),
+      );
+    }
+
     return Scaffold(
       backgroundColor: backgroundColor3,
       appBar: header(),
+      bottomNavigationBar: chatInput(),
     );
   }
 }
