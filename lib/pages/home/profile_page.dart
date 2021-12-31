b904db51fd4ac540c06af71ad12dbe72988214c0
sabilimaulana <spacysbl.learn@gmail.com>
2021-12-31 20:33:05 +0700

    Slicing : Profile Page Bagian Header

--- lib/pages/home/profile_page.dart
+++ lib/pages/home/profile_page.dart
@@ -4,13 +4,62 @@ import 'package:shamo/theme.dart';
 class ProfilePage extends StatelessWidget {
   const ProfilePage({Key? key}) : super(key: key);
 
+  Widget header() {
+    return AppBar(
+      backgroundColor: backgroundColor1,
+      automaticallyImplyLeading: false,
+      elevation: 0,
+      flexibleSpace: SafeArea(
+        child: Container(
+          padding: EdgeInsets.all(
+            defaultMargin,
+          ),
+          child: Row(
+            children: [
+              ClipOval(
+                child: Image.asset(
+                  'assets/image_profile.png',
+                  width: 64,
+                ),
+              ),
+              const SizedBox(
+                width: 16,
+              ),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Hallo, Alex',
+                      style: primaryTextStyle.copyWith(
+                        fontSize: 24,
+                        fontWeight: bold,
+                      ),
+                    ),
+                    Text(
+                      '@alexkeinn',
+                      style: subtitleTextStyle.copyWith(
+                        fontSize: 16,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Image.asset(
+                'assets/button_exit.png',
+                width: 20,
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        'Profile Page',
-        style: primaryTextStyle.copyWith(fontSize: 18),
-      ),
+    return Column(
+      children: [header()],
     );
   }
 }
