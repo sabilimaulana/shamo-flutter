ceddb934b6e2632bc376f31f3b65b14421bdd4ca
sabilimaulana <spacysbl.learn@gmail.com>
2022-01-01 19:25:21 +0700

    Slicing : Edit Profile Page Bagian Header

---
+++ lib/pages/edit_profile_page.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:shamo/theme.dart';
+
+class EditProfilePage extends StatelessWidget {
+  const EditProfilePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    PreferredSizeWidget header() {
+      return AppBar(
+        leading: IconButton(
+          icon: const Icon(Icons.close),
+          onPressed: () => Navigator.pop(context),
+        ),
+        backgroundColor: backgroundColor1,
+        elevation: 0,
+        centerTitle: true,
+        title: const Text('Edit Profile'),
+        actions: [
+          IconButton(
+            onPressed: () => {},
+            icon: Icon(
+              Icons.check,
+              color: priceColor,
+            ),
+          )
+        ],
+      );
+    }
+
+    Widget content() {
+      return Container(
+        width: double.infinity,
+        padding: EdgeInsets.symmetric(
+          horizontal: defaultMargin,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              width: 100,
+              height: 100,
+              margin: EdgeInsets.only(top: defaultMargin),
+              decoration: const BoxDecoration(
+                shape: BoxShape.circle,
+                image: DecorationImage(
+                  image: AssetImage('assets/image_profile.png'),
+                ),
+              ),
+            )
+          ],
+        ),
+      );
+    }
+
+    return Scaffold(
+      backgroundColor: backgroundColor3,
+      appBar: header(),
+      body: content(),
+    );
+  }
+}
