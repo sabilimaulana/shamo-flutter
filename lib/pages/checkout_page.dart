4833bd055e350a3068f1c3a9bd6ec88494ce33e7
sabilimaulana <spacysbl.learn@gmail.com>
2022-01-07 22:35:09 +0700

    Slicing : Checkout Page Bagian Items

--- lib/pages/checkout_page.dart
+++ lib/pages/checkout_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:shamo/theme.dart';
+import 'package:shamo/widgets/checkout_card.dart';
 
 class CheckoutPage extends StatelessWidget {
   const CheckoutPage({Key? key}) : super(key: key);
@@ -17,9 +18,36 @@ class CheckoutPage extends StatelessWidget {
       );
     }
 
+    Widget content() {
+      return ListView(
+        padding: EdgeInsets.symmetric(horizontal: defaultMargin),
+        children: [
+          // NOTE : LIST ITEMS
+          Container(
+            margin: EdgeInsets.only(top: defaultMargin),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'List Items',
+                  style: primaryTextStyle.copyWith(
+                    fontSize: 16,
+                    fontWeight: medium,
+                  ),
+                ),
+                const CheckoutCard(),
+                const CheckoutCard(),
+              ],
+            ),
+          )
+        ],
+      );
+    }
+
     return Scaffold(
       backgroundColor: backgroundColor3,
       appBar: header(),
+      body: content(),
     );
   }
 }
