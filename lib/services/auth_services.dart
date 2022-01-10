660e6151fd5751471cbd2bf1bed49a20f5e2e20e
sabilimaulana <spacysbl.learn@gmail.com>
2022-01-10 21:41:18 +0700

    Integrasi : Menambahkan Auth Service

---
+++ lib/services/auth_services.dart
@@ -0,0 +1,34 @@
+import 'dart:convert';
+import 'package:shamo/models/user_model.dart';
+import 'package:http/http.dart' as http;
+
+class AuthService {
+  String baseUrl = 'https://shamo-backend.buildwithangga.id/api';
+
+  Future<UserModel> register(
+      {required String name,
+      required String username,
+      required String email,
+      required String password}) async {
+    Uri url = '$baseUrl/register' as Uri;
+    var headers = {'Content-Type': 'application/json'};
+    var body = jsonEncode({
+      'name': name,
+      'username': username,
+      'email': email,
+      'password': password
+    });
+
+    var response = await http.post(url, headers: headers, body: body);
+
+    if (response.statusCode == 200) {
+      var data = jsonDecode(response.body)['data'];
+      UserModel user = UserModel.fromJson(data['user']);
+      user.token = 'Bearer ' + data['access_token'];
+
+      return user;
+    } else {
+      throw Exception('Gagal Register');
+    }
+  }
+}
