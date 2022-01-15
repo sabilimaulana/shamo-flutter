71cf699e8a039ec57b4641242e73a7636b0935b8
sabilimaulana <spacysbl.learn@gmail.com>
2022-01-15 11:02:31 +0700

    Integrasi : Menambahkan Model Product, Category, dan Gallery

---
+++ lib/models/product_model.dart
@@ -0,0 +1,54 @@
+import 'package:shamo/models/category_model.dart';
+import 'package:shamo/models/gallery_model.dart';
+
+class ProductModel {
+  late int id;
+  late String name;
+  late double price;
+  late String description;
+  late String tags;
+  late CategoryModel category;
+  late DateTime createdAt;
+  late DateTime updatedAt;
+  late List<GalleryModel> galleries;
+
+  ProductModel({
+    this.id = 0,
+    this.name = '',
+    this.price = 0,
+    this.description = '',
+    this.tags = '',
+    required this.category,
+    required this.createdAt,
+    required this.updatedAt,
+    required this.galleries,
+  });
+
+  ProductModel.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    name = json['name'];
+    price = double.parse(json['price'].toString());
+    description = json['description'];
+    tags = json['tags'];
+    category = CategoryModel.fromJson(json['category']);
+    createdAt = DateTime.parse(json['created_at']);
+    updatedAt = DateTime.parse(json['updated_at']);
+    galleries = json['galleries']
+        .map((gallery) => GalleryModel.fromJson(gallery))
+        .toList();
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'name': name,
+      'price': price,
+      'description': description,
+      'tags': tags,
+      'category': category.toJson(),
+      'galleries': galleries.map((gallery) => gallery.toJson()).toList(),
+      'createdAt': createdAt.toString(),
+      'updatedAt': updatedAt.toString(),
+    };
+  }
+}
